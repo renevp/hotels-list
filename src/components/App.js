185534fd8ce103b0618949e83aa16d7c8691a19b
Rene Vallenilla <rene.vallenilla@gmail.com>
2017-02-26 16:32:39 +1100

    Cleaned up

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
 // This component handles the App template used on every page.
 import React, {PropTypes} from 'react';
-import Header from './common/Header';
 
 class App extends React.Component {
   render() {
     return (
       <div className="container-fluid ">
-        <Header/>
         {this.props.children}
       </div>
     );
--- src/selectors/selectors.js
+++
@@ -1,8 +0,0 @@
-export function sortFormattedForDropdown(sortFilters) {
-  return sortFilters.map(sort => {
-    return {
-      value: sort.id,
-      text: sort.firstName + ' ' + sort.lastName
-    };
-  });
-}
